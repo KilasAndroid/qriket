4616809fa43c7e5f26f309c4f20d19dc3154cf6f
KilasAndroid <41746205+KilasAndroid@users.noreply.github.com>
2019-05-09 12:14:00 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "xxxxxxxx@gmail.com";
+$password = "xxxxxxxx";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "0";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "1";
+
+
+
